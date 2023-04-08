a765c7ef26e970cdcfc85deeccee9cb2ddf988bc
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-04-08 23:45:00 +0300

    Create BaseButton.vue

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+	<button v-on="listeners" v-bind:type="type">
+	<slot></slot>
+	</button>
+</template>
+
+<script >
+	export default {
+		name: 'BaseButton',
+		props: {
+			type: {
+				type: String,
+				default: 'button'
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
