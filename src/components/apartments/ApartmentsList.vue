e122c851dad88276c0aa1852e87aeb3d882d6df9
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-04-12 01:16:27 +0300

    Create ApartmentsList.vue

---
+++ src/components/apartments/ApartmentsList.vue
@@ -0,0 +1,42 @@
+<template>
+	<TheContainer>
+		<div class="apartments-list">
+			<ApartmentsItem
+				v-for='{ id, descr, rating, imageUrl, price} in items'
+				:key='id'
+				:descr='descr'
+				:rating='rating'
+				:imageUrl='imageUrl'
+				:price='price'
+			/>
+		</div>
+	</TheContainer>
+</template>
+
+<script>
+	import ApartmentsItem from './ApartmentsItem.vue'
+	import TheContainer from '../shared/TheContainer.vue'
+
+	export default {
+		name: 'ApartmentsList',
+		components: {
+			ApartmentsItem,
+			TheContainer,
+		},
+		props: {
+			items: {
+				type: Array,
+				default: () => []
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.apartments-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -15px;
+  margin-right: -15px;
+}
+</style>
