5568b36fcaefe9d03c3421a2fd77438a86c18f06
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-04-23 00:03:50 +0300

    Create CustomInput.vue

---
+++ src/components/shared/CustomInput.vue
@@ -0,0 +1,33 @@
+<template>
+	<input class="custom-input" v-model.trim='price'/>
+</template>
+
+<script>
+	export default {
+		name: "CustomInput",
+		data() {
+			return {
+				price: ""
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables.scss';
+
+.custom-input {
+	height: 40px;
+	max-width: 220px;
+	width: 100%;
+  border: 2px solid $main-color;
+  font-size: 18px;
+  outline: none;
+  line-height: inherit;
+  padding: 8px 15px;
+
+	&::placeholder {
+		color: inherit;
+	}
+}
+</style>
