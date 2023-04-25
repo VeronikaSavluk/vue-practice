2de6e019d64054483dcb8f57d36d37f55d74f101
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-04-26 00:09:28 +0300

    Create TheHeader.vue

---
+++ src/components/shared/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+	<header class='header'>
+		<TheContainer>
+			<TheLogo />
+		</TheContainer>
+	</header>
+</template>
+
+<script>
+	import TheContainer from './TheContainer'
+	import TheLogo from '../TheLogo.vue'
+
+	export default {
+		name: 'TheHeader',
+		components: {
+			TheContainer,
+			TheLogo
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import '../../assets/scss/variables.scss';
+
+	.header {
+		height: 60px;
+		background-color: $main-background-color;
+	}
+</style>
