237937b9c876023bc4f58bb625b679e73a99c6f5
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-04-26 00:09:12 +0300

    Create TheLogo.vue

---
+++ src/components/TheLogo.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<img src='image/logo.png' alt='logo' class='logo' />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'TheLogo'
+	}
+</script>
+
+<style lang="scss" scoped>
+	.logo {
+		height: 28px;
+		margin-left: 128px;
+		margin-top: 16px;
+		margin-bottom: 16px;
+	}
+</style>
