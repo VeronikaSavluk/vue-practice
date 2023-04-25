e02f945766f94ee4356061143bbdafdedabd5d52
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-04-25 21:52:50 +0300

    Create TheFooter.vue

---
+++ src/components/shared/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+	<footer class='footer'></footer>
+</template>
+
+<script>
+	export default {
+		name: 'TheFooter'
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import '../../assets/scss/variables.scss';
+
+.footer {
+	height: 30px;
+	background-color: $main-background-color;
+}
+</style>
