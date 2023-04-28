8da114e2eb2a2cfe4d4207b0d4f1dff1e105d24b
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-04-28 22:47:55 +0300

    created reviews of apartment

---
+++ src/components/reviews/reviews-item/ReviewsHeading.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="reviews-heading">
+		<ReviewsAvatar class="reviews-heading__avatar"/>
+		<div class="reviews-heading__content">
+			<span class="reviews-heading__author">{{ author }}</span>
+			<Rating :rating="rating"/>
+		</div>
+	</div>
+</template>
+
+<script>
+import Rating from '../../StarRating.vue';
+import ReviewsAvatar from './ReviewsAvatar.vue'
+
+	export default {
+		name:'ReviewsHeading',
+		components: {
+			ReviewsAvatar,
+			Rating
+		},
+		props: {
+			author: {
+				type: String,
+				required: true
+			},
+			rating: {
+				type: Number,
+				default: 0
+			}
+		}
+	}
+</script>
+	
+<style lang="scss" scoped>
+	.reviews-heading {
+  display: flex;
+  align-items: center;
+  &__avatar {
+    margin-right: 18px;
+  }
+  &__content {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
