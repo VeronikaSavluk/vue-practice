6ab3c1bdcc66f30a91689fa8b58434c5cde96e61
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-04-28 22:43:55 +0300

    Create ApartmentInfo.vue

---
+++ src/components/apartments/ApartmentInfo.vue
@@ -0,0 +1,49 @@
+<template>
+	<article class="apartment-info">
+		<div class="apartment-info__heading">
+			<h2 class="apartment-info__title">{{ apartment.title }}</h2>
+			<StarRating :rating='apartment.rating'/>
+		</div>
+		<img :src="apartment.imageUrl" alt="" class="apartment-info__photo" />
+		<p class="apartment-info__description">{{ apartment.descr }}</p>
+	</article>
+</template>
+
+<script>
+	import StarRating from '@/components/StarRating.vue';
+
+	export default {
+		name: 'ApartmentInfo',
+		components: {
+			StarRating
+		},
+		props: {
+			apartment: {
+				type: Object,
+				required: true
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.apartment-info {
+  &__heading {
+    display: flex;
+  }
+  &__title {
+    font-size: 20px;
+    font-weight: 700;
+    text-transform: capitalize;
+    margin-bottom: 20px;
+    margin-right: 20px;
+  }
+  &__photo {
+    width: 730px;
+  }
+  &__description {
+    line-height: 1.3;
+    margin-top: 30px;
+  }
+}
+</style>
