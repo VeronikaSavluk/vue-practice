c638804da4385f1675b539fe4dc1ecad02688272
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-05-05 00:09:17 +0300

    Update CustomInput.vue

--- src/components/shared/CustomInput.vue
+++ src/components/shared/CustomInput.vue
@@ -1,5 +1,15 @@
 <template>
-	<input class="custom-input" v-model.trim='price'/>
+	<div class="wrapper-input">
+		<input
+		v-bind="$attrs"
+		@blur="blurHandler"
+		:value="modelValue"
+		@input="$emit('update:modelValue',$event.target.value)"
+		class="custom-input"
+		:class="!isValid && `custom-input--error`"
+		/>
+		<span v-if="!isValid" class="custom-input__error-message">{{ error }}</span>
+	</div>
 </template>
 
 <script>
@@ -7,18 +17,84 @@
 		name: "CustomInput",
 		data() {
 			return {
-				price: ""
+				isValid: true,
+				error: '',
+				isFirstInput: true,
 			}
 		},
+		props: {
+			modelValue: {
+				type: String || Number,
+				default: ''
+			},
+			errorMessage: {
+				type: String,
+				default: '',
+			},
+			rules: {
+				type: Array,
+				default: () => [],
+			}
+		},
+		emits: ['update:modelValue'],
+		inheritAttrs: false,
+		inject: {
+			form: {
+				default: null,
+			}
+		},
+		watch: {
+			value(){
+				if(this.isFirstInput) return;
+
+				this.validate();
+			}
+		},
+		mounted(){
+			if(!this.form) return;
+
+			this.form.registerInput(this);
+		},
+		beforeUnmount(){
+			if(!this.form) return;
+
+			this.form.unRegisterInput(this);
+		},
+		methods:{
+			validate(){
+				this.isValid = this.rules.every(rule => {
+					const { hasPassed, message } = rule(this.modelValue);
+					if(!hasPassed){
+						this.error = message || this.errorMessage;
+					}
+					return hasPassed;
+				});
+				return this.isValid;
+			},
+			blurHandler(){
+				if(this.isFirstInput){
+					this.validate();
+				}
+				this.isFirstInput = false
+			},
+			reset(){
+				this.isFirstInput = true;
+				this.isValid = true;
+				this.$emit('update:modelValue', '');
+			}
+		}
 	}
 </script>
 
 <style lang="scss" scoped>
 @import '../../assets/scss/variables.scss';
 
+.wrapper-input {
+  position: relative;
+  display: inline-flex;
+}
 .custom-input {
 	height: 40px;
-	max-width: 220px;
 	width: 100%;
   border: 2px solid $main-color;
   font-size: 18px;
@@ -29,5 +105,18 @@
 	&::placeholder {
 		color: inherit;
 	}
+
+	&--error {
+    border-color: red;
+  }
+  &__error {
+    position: absolute;
+    top: 100%;
+    right: 0;
+    width: 100%;
+    font-size: 12px;
+    color: red;
+    line-height: 1.3;
+  }
 }
 </style>
