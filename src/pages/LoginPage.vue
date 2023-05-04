c924683522e63469ca88c2b5c8eb224af80a81f8
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-05-04 19:46:59 +0300

    Create LoginPage.vue

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,24 @@
+<template>
+	<main class="login-page">
+		<AuthSection>
+			<LoginForm />
+		</AuthSection>
+	</main>
+</template>
+
+<script>
+	import LoginForm from '../components/auth/login'
+	import AuthSection from '@/components/auth/AuthSection.vue';
+	
+	export default {
+		name: 'LoginPage',
+		components: {
+			AuthSection,
+			LoginForm
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
