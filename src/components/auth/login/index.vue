d2fef8f93659560f0931633b532e8859159cf5ae
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-05-04 19:46:40 +0300

    created login form

---
+++ src/components/auth/login/index.vue
@@ -0,0 +1,94 @@
+<template>
+	<AuthContainer class="login">
+		<TheForm ref='form' @submit.prevent="handleSubmit" class="login__form">
+			<CustomInput
+			v-model="formData.email"
+			@input="event => formData.email = event.target.value"
+			name="email"
+			placeholder="Email"
+			:rules="emailRules"
+			class="login__input"
+			/>
+			<CustomInput
+			v-model="formData.password"
+			@input="event => formData.password = event.target.value"
+			type="password"
+			name="password"
+			placeholder="Password"
+			:rules="passwordRules"
+			class="login__input"
+			/>
+			<BaseButton type="submit" class="login__btn">Log in</BaseButton>
+		</TheForm>
+	</AuthContainer>
+</template>
+
+<script>
+	import AuthContainer from '../AuthContainer.vue';
+	import TheForm from '../../shared/form'
+	import CustomInput from '../../shared/CustomInput.vue';
+	import BaseButton from '../../BaseButton.vue';
+	import { emailValidation, passwordValidation, isRequired } from '../../../utils/validationRules'
+
+	export default {
+		name: 'LoginForm',
+		components: {
+			AuthContainer,
+			TheForm,
+			CustomInput,
+			BaseButton
+},
+		data(){
+			return{
+				formData: {
+					email: '',
+					password: ''
+				}
+			};
+		},
+		emits: [ "submit" ],
+		computed: {
+			rules(){
+				return {
+					emailValidation,
+					passwordValidation,
+					isRequired
+				};
+			},
+			emailRules() {
+				return [this.rules.isRequired, this.rules.emailValidation]
+			},
+			passwordRules() {
+				return [this.rules.isRequired, this.rules.passwordValidation]
+			}
+		},
+		methods: {
+			handleSubmit(){
+				const isFormValid = this.$refs.form.validate();
+				if(isFormValid) {
+				return this.$emit('submit', this.formData)
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.login {
+  &__form {
+    display: block;
+    flex-direction: column;
+  }
+  &__title {
+    text-align: center;
+  }
+  &__input {
+    margin-bottom: 20px;
+    width: 100%;
+  }
+  &__btn {
+    margin-top: 15px;
+    width: 100%;
+  }
+}
+</style>
