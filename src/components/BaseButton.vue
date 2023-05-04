1f8b3d49055420517861392cd2c0129594e9984c
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-05-05 00:07:29 +0300

    added loading

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,12 +1,32 @@
 <template>
-	<button v-on="listeners" v-bind:type="type" :class="{ btn: true, 'btn--outlined': outlined }">
-	<slot></slot>
+	<button
+		v-bind:type="type"
+		:class="{ btn: true, 'btn--outlined': outlined }"
+		:disabled="loading"
+	>
+		<Loader
+			v-if="loading"
+			width="36"
+			height="36"
+			class="btn__loader"
+		/>
+		<span
+			class="btn__content"
+			:class="contentStyle"
+		>
+			<slot></slot>
+		</span>
 	</button>
 </template>
 
 <script >
+	import Loader from '../components/loaders/Circle.vue'
+
 	export default {
 		name: 'BaseButton',
+		components: {
+			Loader
+		},
 		props: {
 			type: {
 				type: String,
@@ -15,13 +35,25 @@
 			outlined: {
 			type: Boolean,
 			default: false
+			},
+			loading: {
+				type: Boolean,
+				default: false
 			}
 		},
+		computed: {
+			contentStyle(){
+				return {
+					'btn__content--hidden': this.loading
+				}
+			}
+		}
 	}
 </script>
 
 <style lang="scss" scoped>
 .btn {
+		position: relative;
     display: inline-block;
     font-size: 18px;
     background: #FF662D;
@@ -36,5 +68,17 @@
     border: 1px solid #FF662D;
     color: #FF662D;
     }
+		&__content {
+			&--hidden {
+				opacity: 0;
+			}
+		}
+		&__loader{
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
   }
 </style>
