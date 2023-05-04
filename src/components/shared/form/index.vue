30c91029e76477d27cb607b73805b09ad10aa23c
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-05-04 19:49:27 +0300

    created form

---
+++ src/components/shared/form/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<form class="form" v-bind="$attrs" >
+		<slot></slot>
+	</form>
+</template>
+
+<script>
+	export default {
+		name: 'TheForm',
+		provide() {
+			return{
+				form: this,
+			}
+		},
+		inheritAttrs: false,
+		data() {
+			return {
+				inputs: [],
+			}
+		},
+		methods: {
+			registerInput(input){
+				this.inputs.push(input)
+			},
+			unRegisterInput(input){
+				this.inputs.filter(item => item !== input);
+			},
+			validate(){
+				return this.inputs.reduce((isValid,input) => {
+					const isInputValid = input.validate();
+					return isValid && isInputValid;
+				}, true);
+			},
+			reset(){
+				this.inputs.forEach(input => input.reset());
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
