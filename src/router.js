8ac425a0606063c3404c2818869598c2421d3e21
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-05-06 22:42:34 +0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import HomePage from './pages/HomePage.vue'
 import ApartmentPage from './pages/ApartmentPage.vue'
+import LoginPage from './pages/LoginPage.vue'
+import RegistrationPage from './pages/RegistrationPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -9,7 +11,17 @@ const routes = [
 		name: 'home'
 	},
 	{
-		path: '/apartment/:id',
+		path: '/login',
+		component: LoginPage,
+		name: 'login-page'
+	},
+	{
+		path: '/registration',
+		component: RegistrationPage,
+		name: 'registration-page'
+	},
+	{
+		path: '/apartments/:id',
 		component: ApartmentPage,
 		name: 'apartment'
 	}
